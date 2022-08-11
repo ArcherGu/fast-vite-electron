6e87ca273820df9f780729e8316c40b20beb808b
ArcherGu <649601376@qq.com>
2022-08-11 08:49:56 +0800

    chore: add some constants

---
+++ src/main/framework/constants.ts
@@ -0,0 +1,11 @@
+export const PARAMTYPES_METADATA = 'design:paramtypes'
+export const INJECTABLE = 'injectable'
+export const INJECT_NAME = 'inject:name'
+export const INJECT_TYPE = {
+  CLASS: 'class',
+  CUSTOM: 'custom',
+  WINDOW: 'window',
+}
+export const IPC_INVOKE = 'ipc:invoke'
+export const IPC_ON = 'ipc:on'
+
